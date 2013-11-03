b1196b7c3fd8154efa20eaa514b6cec8ec954f08
bornknow108 <bornknovv108@gmail.com>
2013-11-03 16:30:20 +0900

    トップページのタグ処理を追加

---
+++ static/js/top.js
@@ -0,0 +1,35 @@
+jQuery(function($){
+	$('.js-open-tags').click(function(){
+		$.getJSON('/tags', function(tags){
+			$('.js-tags').empty();
+			
+			var $list = $('<ul>');
+			for (var i = 0; i < tags.length; i++) {
+				var $tag = $('<li>').append($('<a class="off" href="#">').text(tags[i]));
+				$tag.find('a').click(function(){
+					if ($(this).hasClass('off')) {
+						$(this).removeClass('off').addClass('on');
+					} else {
+						$(this).removeClass('on').addClass('off');
+					}
+					return false;
+				});
+				$list.append($tag);
+			}
+
+			$('.js-tags').append($list);
+		});
+		return false;
+	});
+
+	$('.js-search').click(function(){
+		$('#result').fadeIn();
+		
+
+		// $.getJSON('/', function(response){
+		// 	// $('#result').empty();
+		// });
+		return false;
+	});
+
+});
